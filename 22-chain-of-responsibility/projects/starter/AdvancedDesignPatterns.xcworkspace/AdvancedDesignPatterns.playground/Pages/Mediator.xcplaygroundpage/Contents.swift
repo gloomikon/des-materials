112949b91f0c5ae7720578b6574ed6c425f46aa7
Nikolay Zhurba <gloomikon@gmail.com>
2021-10-07 18:07:05 +0300

    mediator

--- 22-chain-of-responsibility/projects/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Mediator.xcplaygroundpage/Contents.swift
+++ 22-chain-of-responsibility/projects/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Mediator.xcplaygroundpage/Contents.swift
@@ -18,40 +18,38 @@
  ## Code Example
  */
 // MARK: - Colleague Protocol
-public protocol Colleague: class {
-  func colleague(_ colleague: Colleague?,
-                 didSendMessage message: String)
+public protocol Colleague: AnyObject {
+    func colleague(_ colleague: Colleague?, didSendMessage message: String)
 }
 
 // MARK: - Mediator Protocol
-public protocol MediatorProtocol: class {
-  func addColleague(_ colleague: Colleague)
-  func sendMessage(_ message: String, by colleague: Colleague)
+public protocol MediatorProtocol: AnyObject {
+    func addColleague(_ colleague: Colleague)
+    func sendMessage(_ message: String, by colleague: Colleague)
 }
 
 // MARK: - Colleague
 public class Musketeer {
+    public var name: String
+    public weak var mediator: MediatorProtocol?
 
-  public var name: String
-  public weak var mediator: MediatorProtocol?
+    public init(mediator: MediatorProtocol, name: String) {
+        self.mediator = mediator
+        self.name = name
+        mediator.addColleague(self)
+    }
 
-  public init(mediator: MediatorProtocol, name: String) {
-    self.mediator = mediator
-    self.name = name
-    mediator.addColleague(self)
-  }
-
-  public func sendMessage(_ message: String) {
-    print("\(name) sent: \(message)")
-    mediator?.sendMessage(message, by: self)
-  }
+    public func sendMessage(_ message: String) {
+        print("\(name) sent: \(message)")
+        mediator?.sendMessage(message, by: self)
+    }
 }
 
 extension Musketeer: Colleague {
-  public func colleague(_ colleague: Colleague?,
-                        didSendMessage message: String) {
-    print("\(name) received: \(message)")
-  }
+    public func colleague(_ colleague: Colleague?,
+                          didSendMessage message: String) {
+        print("\(name) received: \(message)")
+    }
 }
 
 // MARK: - Mediator
@@ -60,16 +58,16 @@ public class MusketeerMediator: Mediator<Colleague> {
 }
 extension MusketeerMediator: MediatorProtocol {
 
-  public func addColleague(_ colleague: Colleague) {
-    self.addColleague(colleague, strongReference: true)
-  }
-
-  public func sendMessage(_ message: String,
-                          by colleague: Colleague) {
-    invokeColleagues(by: colleague) {
-      $0.colleague(colleague, didSendMessage: message)
+    public func addColleague(_ colleague: Colleague) {
+        self.addColleague(colleague, strongReference: true)
     }
-  }
+
+    public func sendMessage(_ message: String,
+                            by colleague: Colleague) {
+        invokeColleagues(by: colleague) {
+            $0.colleague(colleague, didSendMessage: message)
+        }
+    }
 }
 
 // MARK: - Example
@@ -88,5 +86,5 @@ aramis.sendMessage("Unus pro omnibus, omnes pro uno!")
 print("")
 
 mediator.invokeColleagues() {
-  $0.colleague(nil, didSendMessage: "Charge!")
+    $0.colleague(nil, didSendMessage: "Charge!")
 }
